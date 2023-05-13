cfc1e303f9dfbc044d6de20a1c5c9f3ddbc339e5
lemonyxk <lemo@gamstudio.net>
2023-05-13 18:35:15 +0800

    1.auto fit render size.

--- k8s/render.go
+++ k8s/render.go
@@ -18,10 +18,73 @@ import (
 	"github.com/lemonyxk/k8s-forward/app"
 	"github.com/lemonyxk/k8s-forward/config"
 	"github.com/lemonyxk/k8s-forward/tools"
+	"github.com/olekukonko/ts"
 )
 
 func Render() {
 	// render
+
+	// get terminal size
+	size, err := ts.GetSize()
+	if err != nil {
+		console.Exit(err)
+	}
+
+	console.Info(fmt.Sprintf("terminal size: %d x %d", size.Col(), size.Row()))
+
+	if size.Col() < 180 {
+		renderSmall()
+	} else {
+		renderBig()
+	}
+}
+
+func renderSmall() {
+	var table = console.NewTable()
+
+	table.Header("SERVICE NAME", "POD NAME", "CLUSTER IP", "POD IP", "TYPE", "PORT")
+
+	var servicesMap = make(map[string][]*config.Service)
+	var servicesList [][]*config.Service
+
+	for i := 0; i < len(app.Record.Services); i++ {
+		if len(app.Record.Services[i].Pod) == 0 {
+			continue
+		}
+
+		servicesMap[app.Record.Services[i].Namespace] =
+			append(servicesMap[app.Record.Services[i].Namespace], app.Record.Services[i])
+	}
+
+	for namespace := range servicesMap {
+		servicesList = append(servicesList, servicesMap[namespace])
+	}
+
+	for i := 0; i < len(servicesList); i++ {
+		for j := 0; j < len(servicesList[i]); j++ {
+			for k := 0; k < len(servicesList[i][j].Pod); k++ {
+				var svc = servicesList[i][j]
+				var pod = svc.Pod[k]
+
+				table.Row(
+					svc.Name,
+					pod.Name,
+					svc.ClusterIP,
+					pod.IP,
+					svc.Type,
+					tools.ServicePortToString(svc.Port),
+				)
+			}
+		}
+		if i != len(servicesList)-1 {
+			table.Row("-", "-", "-", "-", "-", "-")
+		}
+	}
+
+	console.FgRed.Println(table.SortByName("NAMESPACE", 1).Render())
+}
+
+func renderBig() {
 	var table = console.NewTable()
 
 	table.Header("NAMESPACE", "SERVICE NAME", "POD NAME", "AGE", "RTS", "PHASE", "CLUSTER IP", "POD IP", "TYPE", "PORT")
